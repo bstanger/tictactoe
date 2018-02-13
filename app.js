076587860b3920d9c2b8c0976aa7a30132d28223
Becca Stanger <beccastanger@gmail.com>
2018-02-12 23:14:19 -0800

    Finish refactor to separate presentation and model

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function(){
+  
   ////////////////////////////////////////////////////////////////////
   ////////// MODEL ///////
   ////////////////////////////////////////////////////////////////////
@@ -17,6 +18,25 @@
 
   // Click Square ////////////////////////////////////////////
 
+  var updateModelOnSquareClick = function(clickedRow, clickedCol){
+    ++gameState.squaresPlayed;
+    gameState.boardRowData[clickedRow][clickedCol] = gameState.currentPlayToken;
+    if(gameState.squaresPlayed > 4){
+      checkForWinOrTie(gameState.currentPlayToken);
+    }
+    gameState.currentPlayToken = (gameState.currentPlayToken === 'X') ? 'O' : 'X';
+    updateCurrentPlayerDisplay(gameState.currentPlayToken);
+  };
+
+  var checkForWinOrTie = function(playToken){
+    var boardColData = [[gameState.boardRowData[0][0], gameState.boardRowData[1][0], gameState.boardRowData[2][0]],[gameState.boardRowData[0][1], gameState.boardRowData[1][1], gameState.boardRowData[2][1]],[gameState.boardRowData[0][2], gameState.boardRowData[1][2], gameState.boardRowData[2][2]]];
+    checkForRowColStreak(gameState.boardRowData);
+    checkForRowColStreak(boardColData);
+    checkForDiagonalStreak();
+    checkForTie();
+    return;
+  };
+
   var checkForRowColStreak = function(data){
     for(var i = 0; i < data.length; i++){
       var count = 0;
@@ -26,7 +46,7 @@
         }
       }
       if(count > 2){
-        showResultMessage('Player ' + gameState.currentPlayToken + ' Wins!');
+        showResultMessage('win');
         logWinForPlayer(gameState.currentPlayToken);
         return;
       }
@@ -39,42 +59,54 @@
     }
     if ((gameState.boardRowData[0][0] === gameState.currentPlayToken && gameState.boardRowData[2][2] === gameState.currentPlayToken) ||
     (gameState.boardRowData[0][2] === gameState.currentPlayToken && gameState.boardRowData[2][0] === gameState.currentPlayToken)) {
-      showResultMessage('Player ' + gameState.currentPlayToken + ' Wins!');
+      showResultMessage('win');
       logWinForPlayer(gameState.currentPlayToken);
       return;
     }
   };
 
   var checkForTie = function(){
-    // If all squares filled without a win, tie!
     if(gameState.squaresPlayed === 9){
-      showResultMessage('Tie!');
+      showResultMessage('tie');
       return;
     }
   };
 
-  var checkForWinOrTie = function(playToken){
-    var boardColData = [[gameState.boardRowData[0][0], gameState.boardRowData[1][0], gameState.boardRowData[2][0]],[gameState.boardRowData[0][1], gameState.boardRowData[1][1], gameState.boardRowData[2][1]],[gameState.boardRowData[0][2], gameState.boardRowData[1][2], gameState.boardRowData[2][2]]];
-    checkForRowColStreak(gameState.boardRowData);
-    checkForRowColStreak(boardColData);
-    checkForDiagonalStreak();
-    checkForTie();
-    return;
+  ///////////////////////////////
+
+  var handleResetClick = function(){
+    gameState.boardRowData = [[null, null, null],[null, null, null],[null, null, null]];
+    gameState.squaresPlayed = 0;
+    toggleAllSquareEls(true);
+    showResultMessage();
   };
 
-  var updateModelOnSquareClick = function(clickedRow, clickedCol){
-    ++gameState.squaresPlayed;
-    gameState.boardRowData[clickedRow][clickedCol] = gameState.currentPlayToken;
-    if(gameState.squaresPlayed > 4){
-      checkForWinOrTie(gameState.currentPlayToken);
+  var updatePlayerName = function(player, name){
+    var playerParam = player.toLowerCase() + 'Player';
+    gameState[playerParam]['name'] = name;
+  }
+
+  var logWinForPlayer = function(winner){
+    gameState.currentPlayToken = (gameState.currentPlayToken === 'X') ? 'O' : 'X'; // Reset play token so winner starts next game
+    if(winner === 'X'){
+      gameState.xPlayer.wins++;
+    } else if (winner === 'O'){
+      gameState.oPlayer.wins++;
     }
-    gameState.currentPlayToken = (gameState.currentPlayToken === 'X') ? 'O' : 'X';
-    updateCurrentPlayerDisplay(gameState.currentPlayToken);
+    updateWinsDisplay(winner);
   };
 
-  var updateCurrentPlayerDisplay = function(newCurrentPlayer){
-    document.getElementsByClassName('js-current-player')[0].innerHTML = newCurrentPlayer;
-  };
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////// CONTROLLER ///////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  var boardSqs = document.getElementsByClassName('board__sq');
+  var resultMsgEl = document.getElementsByClassName('result-msg')[0];
+  var boardEl = document.getElementsByClassName('board')[0];
+
+  // Click Events ////////////////////////////////////////////
 
   var handleSquareClick = function(event){
     var clickedSq = event.target;
@@ -89,18 +121,6 @@
     updateModelOnSquareClick(clickedRow, clickedCol);
   };
 
-  var handleResetClick = function(){
-    gameState.boardRowData = [[null, null, null],[null, null, null],[null, null, null]];
-    gameState.squaresPlayed = 0;
-    for(var i = 0; i < boardSqs.length; i++){
-      boardSqs[i].innerHTML = "";
-      boardSqs[i].disabled = false;
-    }
-    showResultMessage();
-  };
-
-  // Name Editing ////////////////////////////////////////////
-
   var editName = function(event){
     document.querySelectorAll('.score-card header')[0].classList.remove('is-editing-name');
     document.querySelectorAll('.score-card header')[1].classList.remove('is-editing-name');
@@ -112,48 +132,53 @@
     var headerElement = document.querySelectorAll('.score-card header.is-editing-name');
     if(event.keyCode === 13 && headerElement.length === 1){
       var inputElement = headerElement[0].getElementsByClassName('score-card__name-input')[0];
-      if(inputElement.value !== ""){
-        if(inputElement.dataset.player === "X"){
-          gameState.xPlayer.name = inputElement.value;
-        } else if (inputElement.dataset.player === "O"){
-          gameState.oPlayer.name = inputElement.value;
-        }
-        headerElement[0].getElementsByClassName('score-card__name')[0].innerHTML = inputElement.value;
+      var inputtedName = inputElement.value;
+      if(inputtedName !== ""){
+        updatePlayerName(inputElement.dataset.player, inputtedName);
+        headerElement[0].getElementsByClassName('score-card__name')[0].innerHTML = inputtedName;
         headerElement[0].classList.remove('is-editing-name');
       }
     }
   }
 
-  // End of Game ////////////////////////////////////////////
+  // Effects ///////////////////////////////////////////////
 
-  var logWinForPlayer = function(winner){
-    gameState.currentPlayToken = (gameState.currentPlayToken === 'X') ? 'O' : 'X'; // Reset play token so winner starts next game
-    if(winner === 'X'){
-      gameState.xPlayer.wins++;
-      document.querySelector('.score-card__score[data-player="X"]').innerHTML = gameState.xPlayer.wins;
-    } else if (winner === 'O'){
-      gameState.oPlayer.wins++;
-      document.querySelector('.score-card__score[data-player="O"]').innerHTML = gameState.oPlayer.wins;
+  var updateCurrentPlayerDisplay = function(newCurrentPlayer){
+    document.getElementsByClassName('js-current-player')[0].innerHTML = newCurrentPlayer;
+  };
+
+  var updateWinsDisplay = function(winner){
+    var currentWinCt = (winner === 'X') ? gameState.xPlayer.wins : gameState.oPlayer.wins;
+    document.querySelector('.score-card__score[data-player="' + winner + '"]').innerHTML = currentWinCt;
+  };
+
+  var toggleAllSquareEls = function(setToEnabled){
+    for(var i = 0; i < boardSqs.length; i++){
+      if (setToEnabled){
+        boardSqs[i].innerHTML = "";
+      }
+      boardSqs[i].disabled = setToEnabled ? false : true;
     }
   };
 
-  var showResultMessage = function(displayText){
-    if(displayText){
-      document.getElementsByClassName('result-msg')[0].innerHTML = displayText;
-      document.getElementsByClassName('result-msg')[0].classList.add('is-shown');
-      document.getElementsByClassName('board')[0].classList.add('with-game-over');
-    } else {
-      document.getElementsByClassName('result-msg')[0].innerHTML = '';
-      document.getElementsByClassName('result-msg')[0].classList.remove('is-shown');
-      document.getElementsByClassName('board')[0].classList.remove('with-game-over');
+  var showResultMessage = function(outcome){
+    if(!outcome){
+      resultMsgEl.innerHTML = '';
+      resultMsgEl.classList.remove('is-shown');
+      boardEl.classList.remove('with-game-over');
+      return;
     }
+    if(outcome === 'win'){
+      var player = gameState.currentPlayToken.toLowerCase() + 'Player';
+      var playerName = gameState[player]['name'];
+      resultMsgEl.innerHTML = playerName + ' Wins!';
+    } else if (outcome === 'tie'){
+      resultMsgEl.innerHTML = 'Tie!';
+    }
+    resultMsgEl.classList.add('is-shown');
+    boardEl.classList.add('with-game-over');
+    toggleAllSquareEls(false);
   };
-
-  ////////////////////////////////////////////////////////////////////
-  ////////// CONTROLLER ///////
-  ////////////////////////////////////////////////////////////////////
-
-  var boardSqs = document.getElementsByClassName('board__sq');
 
   // Render ////////////////////////////////////////////
 
